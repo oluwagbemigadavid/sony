e9bbc64cbca23fa61cd6bb0b6a0b72603345a04a
Oluwagbemiga David <39445905+burnvermin@users.noreply.github.com>
2022-08-19 17:50:11 +0100

    Project Started

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <img :src="images.bg" alt="" id="backgroundImg">
+    <img :src="images.hpBg" alt="" id="HPBig">
+    <div class="overlay"></div>
+    <div class="section-01">
+      <img :src="images.hp" alt="" id="hp">
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import hpBg from '@/assets/HP-BG.png'
+import hp from '@/assets/HPBG.png'
+import bg from '@/assets/BG.png'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      images: {
+        hpBg: hpBg,
+        bg: bg,
+        hp: hp
+      }
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  #backgroundImg  {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+  #HPBig  {
+    height: 100%;
+    position: absolute;
+    left: -35%;
+    top: -40%;
+    transform: scale(1.2);
+  }
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 20%;
+    background: linear-gradient( to bottom, rgba(#000, 1) 5%, rgba(#000, 0));
+    z-index: 2;
+  }
+  .section-01 {
+    position: relative;
+    #hp {
+      margin-top: 20%;
+      transform: scale(1.2);
+    }
+  }
+}
+</style>
